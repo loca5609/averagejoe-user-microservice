c8d45a03ecc62da320024864d1dbd1d3f99a5cf2
ああああああああ\Yayoi <no>
2018-12-03 16:36:19 -0800

    .

--- src/main/java/edu/averagejoecoffeeco/userdb/UserdbApplication.java
+++ src/main/java/edu/averagejoecoffeeco/userdb/UserdbApplication.java
@@ -2,13 +2,14 @@ package edu.averagejoecoffeeco.userdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserdbApplication {
 
-	public static void main(String[] args) {
-        
-		SpringApplication.run(UserdbApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(UserdbApplication.class, args);
+    }
 }
